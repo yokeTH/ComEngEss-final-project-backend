0a2b28757ed9e16f4e704e428b59ff9013c9286d
yokeTH <sern.dev@gmail.com>
2024-04-13 14:57:09 +0700

    add devDependencies file

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
-app.get('/', (req, res)=>{
-  res.json({status: 'ok'})
-})
+app.use(express.urlencoded({ extended: true }));
+app.get('/', (req, res) => {
+  res.json({ status: 'ok' });
+});
 
 export default app;

cc6fb1cb699a2fae8813771b2231d8cb6d83a761
yokeTH <sern.dev@gmail.com>
2024-04-14 22:01:02 +0700

    turn to typescript

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.json({ status: new Date(Date.now()).toTimeString() });
 });
 

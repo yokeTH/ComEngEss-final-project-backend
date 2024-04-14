80d70c59c6f46af7f5492a3fd62345b4841e66a5
yokeTH <sern.dev@gmail.com>
2024-04-14 20:41:02 +0700

    done

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
-  res.json({ status: 'ok' });
+  res.json({ status: new Date(Date.now()).toTimeString() });
 });
 
 export default app;

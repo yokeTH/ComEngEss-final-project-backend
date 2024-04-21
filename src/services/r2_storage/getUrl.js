1c4772ef242a773a9ffeac294618708a63704ebf
dostayesky <brightkav2@gmail.com>
2024-04-22 00:55:43 +0700

    update geturl

--- src/services/r2_storage/getUrl.js
+++ src/services/r2_storage/getUrl.js
@@ -2,7 +2,7 @@ import { join } from 'path';
 const baseUrl = process.env.R2_PUBLIC_HOST || '';
 
 async function getUrl(key) {
-  return 'https://' + join(baseUrl, key);
+  return 'https://' + baseUrl +'/'+key;
 }
 
 export default getUrl;

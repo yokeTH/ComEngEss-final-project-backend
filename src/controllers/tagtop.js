223e8b5912093c076392c0ba2939ed85ef658a14
yokeTH <sern.dev@gmail.com>
2024-04-22 18:21:31 +0700

    update github action and lint fix

--- src/controllers/tagtop.js
+++ src/controllers/tagtop.js
@@ -1,37 +1,37 @@
-import { SuccessResponseDto } from "../dtos/response.js";
-import { HttpClientError } from "../enums/http.js";
-import HttpException from "../exceptions/httpException.js";
-import { Tag, Topic } from "../models/dbShema.js";
-import { authorize } from "../utils/authorizer.js";
+import { SuccessResponseDto } from '../dtos/response.js';
+import { HttpClientError } from '../enums/http.js';
+import HttpException from '../exceptions/httpException.js';
+import { Tag, Topic } from '../models/dbShema.js';
+import { authorize } from '../utils/authorizer.js';
 
-export const getTags = async(req, res,next)=>{
-    try{
-        const { authorization } = req.headers;
-        if (!authorization) throw new HttpException('require authorization', HttpClientError.BadRequest);
-        await authorize(authorization);
-        const tags = await Tag.find({}).populate('post').exec();
-        console.log(tags)
-        const tagsNameDup = tags.map((tag)=>tag.toObject().name);
-        console.log(tagsNameDup)
-        const tagsName = [...new Set(tagsNameDup)];
-        res.json(new SuccessResponseDto(tagsName));
-    }catch(e){
-        console.log(e)
-        next(e)
-    }
-}
+export const getTags = async (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
+    if (!authorization) throw new HttpException('require authorization', HttpClientError.BadRequest);
+    await authorize(authorization);
+    const tags = await Tag.find({}).populate('post').exec();
+    console.log(tags);
+    const tagsNameDup = tags.map((tag) => tag.toObject().name);
+    console.log(tagsNameDup);
+    const tagsName = [...new Set(tagsNameDup)];
+    res.json(new SuccessResponseDto(tagsName));
+  } catch (e) {
+    console.log(e);
+    next(e);
+  }
+};
 
-export const getTopics = async(req, res,next)=>{
-    try{
-        const { authorization } = req.headers;
-        if (!authorization) throw new HttpException('require authorization', HttpClientError.BadRequest);
-        await authorize(authorization);
-        const topics = await Topic.find({});
-        console.log(topics)
-        const topicsName = topics.map((topic)=>topic.toObject().name);
-        res.json(new SuccessResponseDto(topicsName));
-    }catch(e){
-        console.log(e)
-        next(e)
-    }
-}
+export const getTopics = async (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
+    if (!authorization) throw new HttpException('require authorization', HttpClientError.BadRequest);
+    await authorize(authorization);
+    const topics = await Topic.find({});
+    console.log(topics);
+    const topicsName = topics.map((topic) => topic.toObject().name);
+    res.json(new SuccessResponseDto(topicsName));
+  } catch (e) {
+    console.log(e);
+    next(e);
+  }
+};
--- src/utils/authorizer.js
+++ src/utils/authorizer.js
@@ -6,7 +6,7 @@ import { jwtVerify } from './jwt.js';
 export const authorize = async (token) => {
   console.log(token);
   const payload = await jwtVerify(token, process.env.TOKEN_SECRET);
-  if(!(await User.findById(payload.userId))) throw new HttpException("User's token doesn't exist anymore", HttpClientError.Unauthorized)
+  if (!(await User.findById(payload.userId))) throw new HttpException("User's token doesn't exist anymore", HttpClientError.Unauthorized);
   console.log('token verified');
   console.log(payload);
   return payload;
